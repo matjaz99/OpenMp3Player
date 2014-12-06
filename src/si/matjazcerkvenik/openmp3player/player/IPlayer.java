b54388cdfa982b2a50ed4723a19effd7ba1dc0a7
Matjaz <matjaz99@gmail.com>
2014-12-06 17:05:03 +0100

    javadoc

--- src/si/matjazcerkvenik/openmp3player/player/IPlayer.java
+++ src/si/matjazcerkvenik/openmp3player/player/IPlayer.java
@@ -1,6 +1,12 @@
 package si.matjazcerkvenik.openmp3player.player;
 
-
+/**
+ * Implementation of <code>IPlayer</code> must implement basic functions of every music player: 
+ * play, stop, pause, resume.
+ * 
+ * @author matjaz
+ *
+ */
 public interface IPlayer {
 	
 	public void	play(String filepath);
